9ff8a4dc57056b234e98bde7b20022eeaa9a5d66
Rodrigo DM <rodrigo.embsys@gmail.com>
2024-05-01 18:04:54 -0300

    xamtlitz

--- DiverseRadio/src/main.cpp
+++ DiverseRadio/src/main.cpp
@@ -1,4 +1,9 @@
-#include <main.h>
+#include <Arduino.h>
+
+#include <nrf24.h>
+#include <rfm95.h>
+
+#define DEVICE 0
 
 void init(void){
     Serial.begin(115200);
@@ -22,40 +27,27 @@ void init(void){
 int main(){
     uint8_t buffer[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    pinMode(TESTLED, OUTPUT);
-    pinMode(TESTBUT, INPUT);
-
     init();
 
     if(DEVICE == 0){
-        delay(3000);
         while(1){
-            while(digitalReadFast(TESTBUT) == LOW){
-                delay(100);
-                digitalWriteFast(TESTLED, LOW);
-                delay(100);
-                digitalWriteFast(TESTLED, HIGH);
-            }
             nrf_TX_chirp(0x01);
-            delay(1000);
+            delay(3000);
         }
     }
     else{
         while(1){
-            digitalWriteFast(TESTLED, HIGH);
             nrf_registers(buffer);
-            //Serial.print("0x00 = "); Serial.println(buffer[0]);
-            //Serial.print("0x01 = "); Serial.println(buffer[1]);
-            //Serial.print("0x02 = "); Serial.println(buffer[2]);
-            //Serial.print("0x04 = "); Serial.println(buffer[3]);
-            //Serial.print("0x06 = "); Serial.println(buffer[4]);
+            Serial.print("0x00 = "); Serial.println(buffer[0]);
+            Serial.print("0x01 = "); Serial.println(buffer[1]);
+            Serial.print("0x02 = "); Serial.println(buffer[2]);
+            Serial.print("0x04 = "); Serial.println(buffer[3]);
+            Serial.print("0x06 = "); Serial.println(buffer[4]);
             Serial.print("0x17 = "); Serial.println(buffer[5]);
-            delay(100);
-            digitalWriteFast(TESTLED, LOW);
-            //nrf_RX_read(buffer);
-            //Serial.print("  RX = "); Serial.println(nrf_check_RX_buffer());
-            //Serial.println("");
-            delay(900);
+            nrf_RX_read(buffer);
+            Serial.print("  RX = "); Serial.println(nrf_check_RX_buffer());
+            Serial.println("");
+            delay(1000);
         }
     }
 
--- DiverseRadio/src/main.h
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-#include <nrf24.h>
-#include <rfm95.h>
-
-#define DEVICE 0
-
-#define TESTLED 18
-#define TESTBUT 4

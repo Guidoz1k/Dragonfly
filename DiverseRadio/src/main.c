8450bf1a3b81846babc0027da7906fe1b6a8a328
Rodrigo DM <rodrigo.embsys@gmail.com>
2024-08-27 02:00:07 -0300

    agora ignora canais falhos, canal maximo = 30 nrf bugado

--- DiverseRadio/src/main.c
+++ DiverseRadio/src/main.c
@@ -5,7 +5,9 @@
 // purely for visual relief
 #include "main.h"
 
-// ========== GLOBAL VARIABLES ==========
+#define MAGICMAXIMUM 30 // nrf cannot hop between channels that are too far apart
+
+// ========== GLOBAL VARIABLES AND FUNCTIONS ==========
 
 // incremented roughly every 20µs
 static volatile uint64_t time_counter = 0;
@@ -20,33 +22,162 @@ static uint8_t receiving_buffer[3] = {0};
 static uint8_t active_radio = RADIO_NRF24;
 static uint8_t active_channel = 0;
 
-// ============ AUXILIARY FUNCTIONS ============
+// BASE SYSTEM-only global variables and functions
+#ifdef ENV_BASE
+    static bool radio1_status[NRF_MAX + 1] = {0};
+    static bool radio2_status[RFM_MAX + 1] = {0};
+    static uint8_t radio1_available_channels = MAGICMAXIMUM - 1;//NRF_MAX; // doesn't include CH0
+    static uint8_t radio2_available_channels = MAGICMAXIMUM - 1;//RFM_MAX; // doesn't include CH0
+    static bool radio_has_channels = true;
 
-bool ping_nrf(void){
-    bool test = false;
+    void var_init(void){
+        uint8_t i = 0;
+    
+        for(i = 0; i <= NRF_MAX; i++){
+            radio1_status[i] = true;
+        }
+        for(i = 0; i <= RFM_MAX; i++){
+            radio2_status[i] = true;
+        }
+        radio1_available_channels = NRF_MAX;
+        radio2_available_channels = RFM_MAX;
+        radio_has_channels = true;
+    }
 
-    nrf_TXtransmit(header);
+    bool ping_nrf(void){
+        bool test = false;
 
-    delay_tick(); // transmission timeout of 10ms
-    delay_tick(); // transmission timeout of 10ms
+        nrf_TXtransmit(header);
 
-    test = nrf_RXreceive(receiving_buffer);
+        delay_tick(); // transmission timeout of 10ms
+        delay_tick(); // transmission timeout of 10ms
 
-    return test;
-}
+        test = nrf_RXreceive(receiving_buffer);
 
-bool ping_rfm(void){
-    bool test = false;
+        return test;
+    }
 
-    rfm_TXtransmit(header);
+    bool ping_rfm(void){
+        bool test = false;
 
-    delay_tick(); // transmission timeout of 10ms
-    delay_tick(); // transmission timeout of 10ms
+        rfm_TXtransmit(header);
 
-    test = rfm_RXreceive(receiving_buffer);
+        delay_tick(); // transmission timeout of 10ms
+        delay_tick(); // transmission timeout of 10ms
 
-    return test;
-}
+        test = rfm_RXreceive(receiving_buffer);
+
+        return test;
+    }
+
+    void complete_test(void){ // not used
+        const uint8_t max_tries = 3;
+        const uint16_t delay_success = 100;
+        const uint16_t delay_failure = 1200;
+        uint8_t i = 0;
+        uint8_t tries = 0;
+
+        serial_write_string("'+' to initiate self-test.", true);
+        while(serial_read_singlechar() != '+'){
+            delay_tick();
+            led_color(0, 0, 0);
+        }
+
+        // RADIO 1 TEST
+        header[0] = RADIO_NRF24;
+        header[1] = 0xFF;
+        for(i = 0; i <= NRF_MAX; i++){
+            nrf_channel(0);
+            header[2] = i;
+
+            tries = 0;
+            while( (ping_nrf() == false) && (tries < max_tries) ){
+                delay_tick();
+                tries++;
+            }
+            /*
+            if(tries == max_tries)
+                serial_write_string("CANT CONNECT NRFCH0", true);
+            */
+
+            nrf_channel(i);
+            header[2] = 0;
+
+            tries = 0;
+            radio1_status[i] = false;
+            while( (ping_nrf() == false) && (tries < max_tries) ){
+                delay_tick();
+                tries++;
+            }
+
+            if(tries != max_tries)
+                radio1_status[i] = true;
+            else
+                delay_milli(delay_failure);
+        
+            delay_milli(delay_success);
+        }
+        // END OF RADIO 1 TEST
+
+        // RADIO 2 TEST
+        header[1] = 0xFF;
+        nrf_channel(0);
+        for(i = 0; i <= RFM_MAX; i++){
+            header[0] = RADIO_RFM95;
+            header[2] = i;
+
+            tries = 0;
+            while( (ping_nrf() == false) && (tries < max_tries) ){
+                delay_tick();
+                tries++;
+            }
+            /*
+            if(tries == max_tries)
+                serial_write_string("CANT CONNECT NRFCH0", true);
+            */
+
+            header[0] = RADIO_NRF24;
+            header[2] = 0;
+            rfm_channel(i);
+
+            tries = 0;
+            radio1_status[i] = false;
+            while( (ping_rfm() == false) && (tries < max_tries) ){
+                delay_tick();
+                tries++;
+            }
+
+            if(tries != max_tries)
+                radio1_status[i] = true;
+            else
+                delay_milli(delay_failure);
+        
+            delay_milli(delay_success);
+        }
+        // END OF RADIO 2 TEST
+
+        serial_write_string("\n\n nRF24 initial test:", true);
+        for(i = 0; i <= NRF_MAX; i++){
+            serial_write_string(" channel: ", false);
+            serial_write_byte(i, DEC, false);
+            serial_write_string(", status:", false);
+            if(radio1_status[i] == true)
+                serial_write_string(" working.", true);
+            else
+                serial_write_string(" INOPERATIVE.", true);
+        }
+        serial_write_string("\n\n RFM95 initial test:", true);
+        for(i = 0; i <= RFM_MAX; i++){
+            serial_write_string(" channel: ", false);
+            serial_write_byte(i, DEC, false);
+            serial_write_string(", status:", false);
+            if(radio2_status[i] == true)
+                serial_write_string(" working.", true);
+            else
+                serial_write_string(" INOPERATIVE.", true);
+        }
+    }
+#endif
 
 // ============ CORE FUNCTIONS ============
 
@@ -67,10 +198,11 @@ void task_core0(void){
         static bool hopping = false;
         static char option = 0;
         static bool transmission_success = true;
-        //static bool radio1_status[NRF_MAX] = {0};
-        //static bool radio2_status[RFM_MAX] = {0};
+
+        delay_milli(1000);
 
         while(1){
+            var_init();
             nrf_channel(0);
             delay_tick();
             rfm_channel(0);
@@ -82,6 +214,7 @@ void task_core0(void){
             serial_write_string("-> (4) Radio 2, Hopping", true);
             serial_write_string("-> (5) Diversity, CH0", true);
             serial_write_string("-> (6) Diversity, Hopping\n ==>", false);
+            option = 0;
             while( (option != '1') && (option != '2') && (option != '3') && (option != '4') && (option != '5') && (option != '6')){
                 option = serial_read_singlechar();
                 delay_milli(10);
@@ -119,14 +252,13 @@ void task_core0(void){
                 future_radio = RADIO_NRF24;
                 break;
             }
-            option = 0;
 
             active_radio = RADIO_NRF24;
             active_channel = 0;
             future_channel_1 = active_channel;
             serial_write_string("\n TESTING! Press '0' to stop.", true);
 
-            while( serial_read_singlechar() != '0'){
+            while( (serial_read_singlechar() != '0') && (radio_has_channels == true)){
                 header[0] = future_radio;
                 header[1] = packet_counter;
                 switch(future_radio){
@@ -138,6 +270,7 @@ void task_core0(void){
                     break;
                 }
 
+                /* DEBUG TEXTS
                 serial_write_string("\n radio: ", false);
                 serial_write_byte(active_radio, DEC, true);
                 serial_write_string(" channel: ", false);
@@ -148,7 +281,9 @@ void task_core0(void){
                 serial_write_byte(header[1], DEC, true);
                 serial_write_string(" 2 - ", false);
                 serial_write_byte(header[2], DEC, true);
+                */
 
+                // RUNS TRANSMISSION
                 switch(active_radio){
                 case RADIO_NRF24:
                     transmission_success = ping_nrf();
@@ -157,9 +292,12 @@ void task_core0(void){
                     transmission_success = ping_rfm();
                     break;
                 }
+                // END OF TRANSMISSION
 
-                //if( (header[0] != receiving_buffer[0]) || (header[1] != receiving_buffer[1]) || (header[2] != receiving_buffer[2]))
-                //    transmission_success = false;
+                /* RFM for some reason reads wrong packets sometimes, makings some packets = 0
+                if( (header[0] != receiving_buffer[0]) || (header[1] != receiving_buffer[1]) || (header[2] != receiving_buffer[2]))
+                    transmission_success = false;
+                */
 
                 if(transmission_success == true){   // channel is working
                     serial_write_string("S", false);
@@ -175,36 +313,84 @@ void task_core0(void){
                         break;
                     }
 
+                    // RADIO DIVERSITY CONFIG
                     if(diversity == true){
                         if(active_radio == RADIO_NRF24)
                             future_radio = RADIO_RFM95;
                         else
                             future_radio = RADIO_NRF24;
                     }
+                    // END OF RADIO DIVERSITY CONFIG
+
+                    // CHANNEL HOPPING CONFIG
                     if(hopping == true){
                         if(future_radio == RADIO_NRF24){
-                            switch(channel_aux){
-                            case 0:
-                                if(future_channel_1 == NRF_MAX)
-                                    channel_aux = 1;
-                                else
-                                    future_channel_1++;
-                                break;
-                            case 1:
-                                if(future_channel_1 == 0)
-                                    channel_aux = 0;
-                                else
-                                    future_channel_1--;
-                                break;
+                            do{
+                                switch(channel_aux){
+                                case 0:
+                                        if(future_channel_1 == MAGICMAXIMUM)
+                                            channel_aux = 1;
+                                        else
+                                            future_channel_1++;
+                                        break;
+                                case 1:
+                                    if(future_channel_1 == 0)
+                                        channel_aux = 0;
+                                    else
+                                        future_channel_1--;
+                                    break;
+                                }
                             }
+                            while(radio1_status[future_channel_1] == false);
                         }
                         else
-                            future_channel_2 = random_n(RFM_MAX);
+                            do{
+                                if(future_channel_2 == MAGICMAXIMUM)
+                                    future_channel_2 = 0;
+                                else
+                                    future_channel_2++;
+                            }
+                            while(radio2_status[future_channel_2] == false);
                     }
+                    // END OF CHANNEL HOPPING CONFIG
                 }
                 else{                               // channel is not working
                     serial_write_string("F", false);
                     packet_failures++;
+                    if( (active_channel != 0) && (hopping == true)){ // never flags CH0 as inoperative
+                        serial_write_string("(", false);
+                        serial_write_byte(active_channel, DEC, false);
+                        serial_write_string(")", false);
+                        switch(active_radio){
+                        case RADIO_NRF24:
+                            radio1_status[active_channel] = false;
+                            if(radio1_available_channels > 1)
+                                radio1_available_channels--;
+                            else
+                                radio_has_channels = false;
+                            break;
+                        case RADIO_RFM95:
+                            radio2_status[active_channel] = false;
+                            if(radio2_available_channels > 1)
+                                radio2_available_channels--;
+                            else
+                                radio_has_channels = false;
+                            break;
+                        }
+                    }
+                    if( (diversity == true) || (hopping == true) || (active_radio == RADIO_RFM95)){
+                        delay_milli(1000);
+                        if( (option == '2') || (option == '4') )
+                            future_radio = RADIO_RFM95;
+                        else
+                            future_radio = RADIO_NRF24;
+                        active_radio = RADIO_NRF24;
+                        active_channel = 0;
+                        future_channel_1 = 0;
+                        future_channel_2 = 0;
+                        nrf_channel(0);
+                        rfm_channel(0);
+                    }
                 }
 
                 if(packet_counter < 255)
@@ -216,8 +402,8 @@ void task_core0(void){
                     packet_counter = 0;
                     packet_failures = 0;
                 }
-                delay_milli(250);
-                }
+                delay_milli(50);
+            }
 
             serial_write_string(" Test finished!\n ", false);
             serial_write_byte(packet_counter, DEC, false);
@@ -234,7 +420,7 @@ void task_core0(void){
         bool reset = false;
         bool radio_status = false;
         static volatile uint64_t timeout_start = 0;      // first occurence of no response
-        static const uint64_t timeout_max = 150000ul;    // maximum timer ticks (20µs) to clear Drone status = 3s
+        static const uint64_t timeout_max = 50000ul;     // maximum timer ticks (20µs) to clear Drone status = 1s
 
         while(1){
             switch(active_radio){
@@ -276,10 +462,11 @@ void task_core0(void){
                     rfm_TXtransmit(header);
                     break;
                 }
-                if( (receiving_buffer[0] == 1) || (receiving_buffer[0] == 2))
-                    active_radio = receiving_buffer[0];
-                else
+                active_radio = receiving_buffer[0];
+                if(active_radio == 0)
                     active_radio = RADIO_NRF24;
+                if(active_radio == 3) 
+                    active_radio = RADIO_RFM95;
                 active_channel = receiving_buffer[2];
                 switch(active_radio){
                 case RADIO_NRF24:
@@ -299,11 +486,13 @@ void task_core0(void){
                     if( (time_counter - timeout_start) >= timeout_max ){
                         timeout_start = 0;
                         active_radio = RADIO_NRF24;
+                        active_channel = 0;
                         nrf_channel(0);
                         rfm_channel(0);
                         led_color(255, 0, 0);
                         delay_tick();
                         led_color(0, 0, 0);
+
                         if(reset == false){
                             serial_write_string("\n RESET", true);
                             reset = true;
